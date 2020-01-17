5d72370c9e6e91eab291449f2d669f20bc38ca41
Rosa Isela Lopez Palma <rosaiselalopezpalma@air-de-rosa>
2020-01-17 09:51:55 -0600

    Updates

---
+++ precache-manifest.525bd8e7393d275b3585f55d7a967541.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "952c570fe9d7458a3d494dd9d63a7e4f",
+    "url": "/tictactochooks/index.html"
+  },
+  {
+    "revision": "c8a7bf74dc447a05e1f3",
+    "url": "/tictactochooks/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "69f47a6adebc329c5bde",
+    "url": "/tictactochooks/static/js/2.c5bbb7bb.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/tictactochooks/static/js/2.c5bbb7bb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c8a7bf74dc447a05e1f3",
+    "url": "/tictactochooks/static/js/main.c5abe6d2.chunk.js"
+  },
+  {
+    "revision": "7a1eab240a2ecfff5a31",
+    "url": "/tictactochooks/static/js/runtime-main.bcefc6d4.js"
+  }
+]);
