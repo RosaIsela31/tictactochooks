322f6799a0a6c9afbb633469915fe20ae530d800
Rosa Isela Lopez Palma <rosaiselalopezpalma@air-de-rosa>
2020-01-18 00:39:05 -0600

    style of the first page changed

--- src/components/rulesOfTheGame/index.js
+++ src/components/rulesOfTheGame/index.js
@@ -5,7 +5,7 @@ const RulesOfTheGame = () => {
   return(
     <div>
       <div className='rules'>
-        <h5 className='rules-h5'>REGLAS DEL JUEGO <br/><br/> Se requieren de 2 jugadores, el primer jugador en tirar es X, <br/> el segundo es O</h5>
+        <h5> Se requieren de 2 jugadores, el primero en tirar es X, el segundo es O</h5>
       </div>
     </div>
   )
